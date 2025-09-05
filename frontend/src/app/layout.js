f3e392ecba79729584c13663abbcb30b8b253966
bl4ck01 <amrouss.issame@gmail.com>
2025-09-06 00:45:33 +0100

    Update

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} bg-black text-gray-100 antialiased overflow-hidden`}
+        className={`${geistSans.variable} ${geistMono.variable} bg-black text-gray-100 antialiased`}
       >
         {children}
         <Toaster richColors position="bottom-center" />
